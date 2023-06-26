6da67dc804792dcbcedd094ed64b48e746059474
alexis-ashby <137818425+alexis-ashby@users.noreply.github.com>
2023-06-26 15:08:51 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
 print("Hello World!")
 print("\t This is my first Git repository on Git Hub.")
+def print_message(name='Python'):
+  print(f"Welcome to the class from {name}!)
+name = "Julia"
+print_message(name)
